75c8c1cd478006740b21c6867800265f808e084e
Petras Malinauskas <petrasmalinauskas96@gmail.com>
2019-07-10 19:29:23 +0300

    Added configs

---
+++ ios-party/ios-party/Config/Config+Api.swift
@@ -0,0 +1,4 @@
+extension Config.Api {
+    static let tokenServiceEndpoint = "http://playground.tesonet.lt/v1/tokens"
+    static let serverServiceEndpoint = "http://playground.tesonet.lt/v1/servers"
+}
